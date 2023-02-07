1c5fe4a91ccbc060837f5f1326b5eb689163ec0f
tamtn2 <77471621+tamtn2@users.noreply.github.com>
2023-02-07 12:03:57 -1000

    Added JSON file

--- my-cytoreact-app/src/App.js
+++ my-cytoreact-app/src/App.js
@@ -1,13 +1,27 @@
 import CytoscapeComponent from 'react-cytoscapejs';
+import data from './data.json'
 
 function App() {
   const elements = [
-    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+    // Nodes
+    { data: { id: 'host1', type: 'rectangle', label: 'Host 1' } },
+    { data: { id: 'host2', type: 'rectangle', label: 'Host 2' } },
+    { data: { id: 'h1c1', parent: 'host1', type: 'ellipse', label: 'core 1' }, position: { x: 215, y: 5 } },
+    { data: { id: 'h1c2', parent: 'host1', type: 'ellipse', label: 'core 2' }, position: { x: 265, y: 5 } },
+    { data: { id: 'h1c3', parent: 'host1', type: 'ellipse', label: 'core 3' }, position: { x: 215, y: 55 } },
+    { data: { id: 'h1c4', parent: 'host1', type: 'ellipse', label: 'core 4' }, position: { x: 265, y: 55 } },
+    { data: { id: 'h2c1', parent: 'host2', type: 'ellipse', label: 'core 1' }, position: { x: 700, y: 105 } },
+    { data: { id: 'h2c2', parent: 'host2', type: 'ellipse', label: 'core 2' }, position: { x: 750, y: 105 } },
+    { data: { id: 'connection', type: 'diamond', label: '' }, position: { x: 500, y: 75 } },
+    { data: { id: 'disk', type: 'rectangle', label: 'Disk' }, position: { x: 15, y: 5 } },
+
+    // Edges
+    { data: { id: 'disk-to-host1', source: 'disk', target: 'host1', label: '' } },
+    { data: { id: 'host1-connection', source: 'host1', target: 'connection', label: 'Link1' } },
+    { data: { id: 'host2-connection', source: 'host2', target: 'connection', label: 'Link2' } },
   ];
   return (
-    <CytoscapeComponent elements={elements} style={ { width: '600px', height: '600px' } } />
+    <CytoscapeComponent elements={elements} style={ { width: '1800px', height: '1800px' } } />
   );
 }
 
