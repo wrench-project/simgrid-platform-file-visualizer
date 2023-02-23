8fc8326ffeb5dfede0341a1461506481294a4194
cathy-kim95 <ckim60@hawaii.edu>
2023-02-23 13:35:31 -1000

    fixed popup

--- my-cytoreact-app/src/App.js
+++ my-cytoreact-app/src/App.js
@@ -1,5 +1,10 @@
 import CytoscapeComponent from 'react-cytoscapejs';
 import FileUploadButton from './FileUpload';
+import popper from 'cytoscape-popper';
+import cytoscape from "cytoscape";
+import "./popper.css";
+
+cytoscape.use(popper);
 
 // Sample data
 const elements = [
@@ -63,14 +68,38 @@ function App() {
     <CytoscapeComponent 
       elements={elements} 
       style={{ width: '1800px', height: '1800px' }} 
-      stylesheet={stylesheet} 
+      stylesheet={stylesheet}
       cy = {cy => {
         cy.on("tap", "node", evt => {
           var node = evt.target;
           console.log("event", evt);
           console.log("target", node.data());
+        })
+        cy.elements().unbind("mouseover");
+        cy.elements().bind("mouseover", (event) => {
+          event.target.popperRefObj = event.target.popper({
+            content: () => {
+              let content = document.createElement("div");
+
+              if (event.target.data("type") !== "ellipse" ) {
+                content.classList.add("popper-div");
+                content.innerHTML = event.target.id();
+              }
+              document.body.appendChild(content);
+              return content;
+            },
+          });
         });
-      }} />
+
+        cy.elements().unbind("mouseout");
+        cy.elements().bind("mouseout", (event) => {
+          if (event.target.popper) {
+            event.target.popperRefObj.state.elements.popper.remove();
+            event.target.popperRefObj.destroy();
+          }
+        });
+      }}
+      />
     </>);
 }
 
