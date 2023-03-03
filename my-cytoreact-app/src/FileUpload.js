5a1359b6e053e373b88af8f4df0c77fdea923005
cathy-kim95 <ckim60@hawaii.edu>
2023-03-02 20:18:48 -1000

    added data structure of elements

--- my-cytoreact-app/src/FileUpload.js
+++ my-cytoreact-app/src/FileUpload.js
@@ -23,12 +23,12 @@ function FileUploadButton() {
             const jsonDataFromXml = new XMLParser().parseFromString(data);
             console.log(jsonDataFromXml);
 
-            const childList = [];
-            iterateJson(jsonDataFromXml, childList);
-            console.log(childList);
+            const elements = [];
+            iterateJson(jsonDataFromXml, elements);
+            console.log(elements);
         }
 
-        function iterateJson(json, childList) {
+        function iterateJson(json, elements) {
             for (const key in json) {
                 if (json.hasOwnProperty(key)) {
                     const value = json[key];
@@ -36,20 +36,49 @@ function FileUploadButton() {
                     if (key === "name") {
                         if (typeof value === "object") {
                             // If the value is an object, recursively iterate through its keys
-                            iterateJson(value, childList);
+                            iterateJson(value, elements);
                         } else {
                             // Ignore DOCTYPE and --
                             if (value !== "DOCTYPE" && value !== "--" && value !== "---") {
-                                const child = {
+                                const element = {
                                     name: value,
                                     attributes: json.attributes
                                 };
-                                childList.push(child);
+                                if (json.attributes.speed) {
+                                    elements.push(
+                                        {
+                                            data: {
+                                                id: json.attributes.id,
+                                                name: element.name,
+                                                speed: json.attributes.speed
+                                            }})
+                                } else if (json.attributes.latency) {
+                                    elements.push(
+                                        {
+                                            data: {
+                                                id: json.attributes.id,
+                                                name: element.name,
+                                                bandwidth: json.attributes.bandwidth,
+                                                latency: json.attributes.latency
+                                            }})
+                                } else if (json.attributes.src) {
+                                    elements.push(
+                                        {
+                                            data: {
+                                                id: json.attributes.id,
+                                                name: element.name,
+                                                source: json.attributes.src,
+                                                target: json.attributes.dst
+                                            }})
+                                } else {
+                                    elements.push({data: {id: json.attributes.id, name: element.name}});
+                                }
                             }
+
                         }
                     } else if (typeof value === "object") {
                         // If the value is an object, recursively iterate through its keys
-                        iterateJson(value, childList);
+                        iterateJson(value, elements);
                     }
                 }
             }
