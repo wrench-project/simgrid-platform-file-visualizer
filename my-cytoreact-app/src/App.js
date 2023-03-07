8eea8a6319642ea66ce68bc2396f0f5e0ce074c8
cathy-kim95 <ckim60@hawaii.edu>
2023-03-07 12:16:53 -1000

    update App.js

--- my-cytoreact-app/src/App.js
+++ my-cytoreact-app/src/App.js
@@ -8,38 +8,6 @@ import {useState} from "react";
 
 cytoscape.use(popper);
 
-// sample data
-const elements = [
-    // Nodes
-    {data: {id: 'host1', type: 'rectangle', label: 'Host 1', size: 'big'}},
-    {data: {id: 'host2', type: 'rectangle', label: 'Host 2'}, style: {'background-color': 'lightgreen'}},
-    {
-        data: {id: 'h1c1', parent: 'host1', type: 'ellipse', label: 'core 1'},
-        position: {x: 215, y: 5},
-        style: {'background-color': 'red'}
-    },
-    {data: {id: 'h1c2', parent: 'host1', type: 'ellipse', label: 'core 2'}, position: {x: 265, y: 5}},
-    {data: {id: 'h1c3', parent: 'host1', type: 'ellipse', label: 'core 3'}, position: {x: 215, y: 55}},
-    {
-        data: {id: 'h1c4', parent: 'host1', type: 'ellipse', label: 'core 4'},
-        position: {x: 265, y: 55},
-        style: {'background-color': 'blue'}
-    },
-    {data: {id: 'h2c1', parent: 'host2', type: 'ellipse', label: 'core 1'}, position: {x: 700, y: 105}},
-    {data: {id: 'h2c2', parent: 'host2', type: 'ellipse', label: 'core 2'}, position: {x: 750, y: 105}},
-    {
-        data: {id: 'connection', type: 'diamond', label: ''},
-        position: {x: 500, y: 75},
-        style: {'background-color': 'purple'}
-    },
-    {data: {id: 'disk', type: 'rectangle', label: 'Disk'}, position: {x: 15, y: 5}},
-
-    // Edges
-    {data: {id: 'disk-to-host1', source: 'disk', target: 'host1', label: ''}},
-    {data: {id: 'host1-connection', source: 'host1', target: 'connection', label: 'Link1'}, style: {width: '10px'}},
-    {data: {id: 'host2-connection', source: 'host2', target: 'connection', label: 'Link2'}},
-];
-
 const stylesheet = [
     {
         selector: 'node',
