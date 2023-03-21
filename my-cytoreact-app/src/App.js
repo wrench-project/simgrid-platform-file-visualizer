ff8a1fb5c60957951d001f8b6d503816850ef6e4
tamtn2 <77471621+tamtn2@users.noreply.github.com>
2023-03-21 11:21:01 -1000

    Merge Iteration with current branch

--- my-cytoreact-app/src/App.js
+++ my-cytoreact-app/src/App.js
@@ -4,71 +4,72 @@ import FileUploadButton from './FileUpload';
 import PopUp from './PopUp';
 
 // sample data
-const elements = [
-    // Nodes
-    // G1
-    {data: {id: 'host1', type: 'rectangle', label: 'Host 1', size: 'big'}},
-    {data: {id: 'h1c1', parent: 'host1', type: 'ellipse', label: 'core 1'},style: {'background-color': 'red'}},
-    {data: {id: 'h1c2', parent: 'host1', type: 'ellipse', label: 'core 2'}},
-    {data: {id: 'h1c3', parent: 'host1', type: 'ellipse', label: 'core 3'}},
-    {data: {id: 'h1c4', parent: 'host1', type: 'ellipse', label: 'core 4'},style: {'background-color': 'blue'}},
+// const elements = [
+//     // Nodes
+//     // G1
+//     {data: {id: 'host1', eleType: 'Host', type: 'rectangle', label: 'Host 1', size: 'big', cores: '4', speed: '43.095Mf'}},
+//     {data: {id: 'h1c1', eleType: 'Core', parent: 'host1', type: 'ellipse', label: 'core 1'},style: {'background-color': 'red'}},
+//     {data: {id: 'h1c2', eleType: 'Core', parent: 'host1', type: 'ellipse', label: 'core 2'}},
+//     {data: {id: 'h1c3', eleType: 'Core', parent: 'host1', type: 'ellipse', label: 'core 3'}},
+//     {data: {id: 'h1c4', eleType: 'Core', parent: 'host1', type: 'ellipse', label: 'core 4'},style: {'background-color': 'blue'}},
 
-    // G2
-    {data: {id: 'host2', type: 'rectangle', label: 'Host 2'}, style: {'background-color': 'lightgreen'}},
-    {data: {id: 'h2c1', parent: 'host2', type: 'ellipse', label: 'core 1'}},
-    {data: {id: 'h2c2', parent: 'host2', type: 'ellipse', label: 'core 2'}},
+//     // G2
+//     {data: {id: 'host2', eleType: 'Host', type: 'rectangle', label: 'Host 2', cores: '2', speed: '66.195Mf'}, style: {'background-color': 'lightgreen'}},
+//     {data: {id: 'h2c1', eleType: 'Core', parent: 'host2', type: 'ellipse', label: 'core 1'}},
+//     {data: {id: 'h2c2', eleType: 'Core', parent: 'host2', type: 'ellipse', label: 'core 2'}},
     
-    // G3
-    {data: {id: 'connection', type: 'diamond', label: ''},style: {'background-color': 'purple'}},
+//     // G3
+//     {data: {id: 'connection', eleType: 'extra', type: 'diamond', label: 'connection'},style: {'background-color': 'purple'}},
     
-    // G4
-    {data: {id: 'disk', type: 'rectangle', label: 'Disk'}},
+//     // G4
+//     {data: {id: 'disk1', eleType: 'disk', type: 'rectangle', label: 'Disk'}},
 
-    // Edges
-    {data: {id: 'disk-to-host1', source: 'disk', target: 'host1', label: 'link3'}},
-    {data: {id: 'host1-connection', source: 'host1', target: 'connection', label: 'Link1'}, style: {width: '10px'}},
-    {data: {id: 'host2-connection', source: 'host2', target: 'connection', label: 'Link2'}},
-];
+//     // Edges
+//     {data: {id: 'disk-to-host1', eleType: 'Link', bandwidth:'44.279125MBps', latency: '59.904us', source: 'disk1', target: 'host1', label: 'link3'}},
+//     {data: {id: 'host1-connection', eleType: 'Link', bandwidth:'11.216125MBps', latency: '59.234us', source: 'host1', target: 'connection', label: 'Link1'}, style: {width: '10px'}},
+//     {data: {id: 'host2-connection', eleType: 'Link', bandwidth:'41.269325MBps', latency: '21.904us', source: 'host2', target: 'connection', label: 'Link2'}},
+// ];
 
-const stylesheet = [
-    {
-        selector: 'node',
-        css: {
-            'shape': 'data(type)',
-            'label': 'data(label)'
-        }
-    },
-    {
-        selector: 'edge',
-        css: {
-            'curve-style': 'bezier',
-            'label': 'data(label)'
-        }
-    },
-    {
-        selector: ':parent',
-        css: {
-            'text-valign': 'top',
-            'text-halign': 'center',
-        }
-    },
-    {
-        selector: ':child',
-        css: {
-            'background-color': 'black'
-        }
-    },
-    {
-        selector: '#disk',
-        css: {
-            'background-image': 'https://cdn-icons-png.flaticon.com/512/227/227889.png',
-            'height': 120,
-            'width': 120,
-            'background-fit': 'contain',
-            'background-color': '#ffffff',
-        }
-    },
-];
+
+// const stylesheet = [
+//     {
+//         selector: 'node',
+//         css: {
+//             'shape': 'data(type)',
+//             'label': 'data(label)'
+//         }
+//     },
+//     {
+//         selector: 'edge',
+//         css: {
+//             'curve-style': 'bezier',
+//             'label': 'data(label)'
+//         }
+//     },
+//     {
+//         selector: ':parent',
+//         css: {
+//             'text-valign': 'top',
+//             'text-halign': 'center',
+//         }
+//     },
+//     {
+//         selector: ':child',
+//         css: {
+//             'background-color': 'black'
+//         }
+//     },
+//     {
+//         selector: '#disk',
+//         css: {
+//             'background-image': 'https://cdn-icons-png.flaticon.com/512/227/227889.png',
+//             'height': 120,
+//             'width': 120,
+//             'background-fit': 'contain',
+//             'background-color': '#ffffff',
+//         }
+//     },
+// ];
 
 const style = 
     {width: '1440px', height: '600px'};
@@ -81,26 +82,35 @@ const layout =
 
 var obj = {};
 
-function App() {
+function App() {   
+    // Modal Handles
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    // Element Handles
+    const [elements, setElements] = useState([]);
+    const handleElements = (newElements) => {
+        setElements(newElements);
+    }
+
     return (
         <>
-            <FileUploadButton/>
+            <FileUploadButton handleElements={handleElements}/>
             <PopUp obj={obj} open={open} close={handleClose}/>
             <CytoscapeComponent
             elements={elements}
             style={style}
-            stylesheet={stylesheet}
+            // stylesheet={stylesheet}
             pan={pan}
             layout={layout}
             cy={cy => {
                 cy.on("tap", evt => {
                     try {
                         obj = evt.target.data();
-                        handleOpen();
+                        if (obj !== null){
+                            handleOpen();
+                        }
                     } catch (error) {
                         console.log("Error; Not a node")
                     }
--- my-cytoreact-app/src/FileUpload.js
+++ my-cytoreact-app/src/FileUpload.js
@@ -1,41 +1,48 @@
-import { useState } from "react";
+import {useState} from "react";
 import XMLParser from 'react-xml-parser';
+import iterateJson from "./IterateJson";
 
-function FileUploadButton() {
-  // Variable selectedFile = File
-	const [selectedFile, setSelectedFile] = useState();
+function FileUploadButton(props) {
+    const [selectedFile, setSelectedFile] = useState();
 
-  // Target file as selectedFile
-	const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-	};
+    // Target file as selectedFile
+    const changeHandler = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
 
-  // Read in file
-  // Returns file content as string
-	const handleSubmission = () => {
-    const reader = new FileReader();
-    reader.readAsText(selectedFile);
-    reader.onload = function(e) {
-      var preParseData = e.target.result;
-      parseData(preParseData);
-  };
 
-  // Parse data
-  // Returns XML obj
-  const parseData = (data) =>{
-    var jsonDataFromXml = new XMLParser().parseFromString(data);
-    console.log(jsonDataFromXml);
-  }
-};
+    // Read in file
+    const handleSubmission = () => {
+        const reader = new FileReader();
+        reader.readAsText(selectedFile);
+        reader.onload = function (e) {
+            var preParseData = e.target.result;
+            parseData(preParseData);
+        };
 
-return(
-   <div style={{ backgroundColor: "lightgray" }}>
-        <input type="file" name="file" onChange={changeHandler} />
-        <div>
-            <button onClick={handleSubmission}>Submit</button>
-        </div>
-    </div>
-	)
+        // Parse data
+        const parseData = (data) => {
+            const jsonDataFromXml = new XMLParser().parseFromString(data);
+
+            // main item to be passed
+            const elements = [];
+
+
+            iterateJson(jsonDataFromXml, elements);
+            console.log(elements);
+
+            props.handleElements(elements);
+        }
+    };
+
+    return (
+      <div style={{ backgroundColor: "lightgray" }}>
+           <input type="file" name="file" onChange={changeHandler} />
+           <div>
+               <button onClick={handleSubmission}>Submit</button>
+           </div>
+       </div>
+    )
 }
 
 export default FileUploadButton;
